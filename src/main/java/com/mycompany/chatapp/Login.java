e85f31651aec84fcd46042b9f52039227d476a54
Student <Student@LabVM2049939.5p4z1o521fyepcpbqd5inzuzjh.jnbx.internal.cloudapp.net>
2026-09-16 22:40:16 +0200

    added a boolean to check cellphone number and for login user details.

--- src/main/java/com/mycompany/chatapp/Login.java
+++ src/main/java/com/mycompany/chatapp/Login.java
@@ -73,4 +73,61 @@ public class Login {
             } else {
             return false;
         }
+    }// Check South African cellphone number
+    public boolean checkCellPhoneNumber() {
+
+        String phonePattern = "^\\+27\\d{9}$";
+
+        return Pattern.matches(phonePattern, cellPhoneNumber);
     }
+
+    // Register user
+    public String registerUser() {
+
+        if (!checkUserName()) {
+
+            return "Username is not correctly formatted; please ensure that your username contains an underscore and is no more than five characters in length.";
+
+        } else if (!checkPasswordComplexity()) {
+
+            return "Password is not correctly formatted; please ensure that the password contains at least eight characters, a capital letter, a number, and a special character.";
+
+        } else if (!checkCellPhoneNumber()) {
+
+            return "Cell number is incorrectly formatted or does not contain an international code; please correct the number and try again.";
+
+        } else {
+
+            return "User registered successfully.";
+        }
+    }
+
+    // Check login details
+    public boolean loginUser(String enteredUsername, String enteredPassword) {
+
+        if (enteredUsername.equals(username)
+                && enteredPassword.equals(password)) {
+
+            return true;
+
+        } else {
+
+            return false;
+        }
+    }
+
+    // Return login status
+    public String returnLoginStatus(String enteredUsername,
+                                    String enteredPassword) {
+
+        if (loginUser(enteredUsername, enteredPassword)) {
+
+            return "Welcome " + firstName + ", " + lastName
+                    + " it is great to see you again.";
+
+        } else {
+
+            return "Username or password incorrect, please try again.";
+        }
+    }
+}
