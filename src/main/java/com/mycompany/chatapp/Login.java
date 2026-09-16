301d30af652a6da7e4657d6788bddcba117baf89
Student <Student@LabVM2049939.5p4z1o521fyepcpbqd5inzuzjh.jnbx.internal.cloudapp.net>
2026-09-16 21:01:44 +0200

    Created the main,login and logintest files

---
+++ src/main/java/com/mycompany/chatapp/Login.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.chatapp;
+
+/**
+ *
+ * @author Student
+ */
+
+public class Login {
+
+    // Variables
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String cellPhoneNumber;
+
+    // Constructor
+    public Login(String firstName, String lastName, String username,
+                 String password, String cellPhoneNumber) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.cellPhoneNumber = cellPhoneNumber;
+    } 
